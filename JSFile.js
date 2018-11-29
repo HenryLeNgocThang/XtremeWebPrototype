38c791f18b5c4783e5738993338ef9a6c96fb191
HenryLeNgocThang <35796147+HenryLeNgocThang@users.noreply.github.com>
2018-11-29 20:58:20 +0100

    commit

--- JSFile.js
+++ JSFile.js
@@ -6,7 +6,6 @@ function showSlides()
 {
     var i;
     var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
 
     for (i = 0; i < slides.length; i++)
     {
@@ -15,15 +14,9 @@ function showSlides()
 
     slideIndex++;
 
-    if (slideIndex > slides.length) {slideIndex = 1}  
+    if (slideIndex > slides.length) {slideIndex = 1}
 
-    for (i = 0; i < dots.length; i++)
-    {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-
-    slides[slideIndex-1].style.display = "block";  
-    dots[slideIndex-1].className += " active";
+    slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 6000); // Change image every 6 seconds
 }
 
@@ -40,4 +33,5 @@ window.onclick = function(event)
 	}
 }
 
-// Change navbar
+// Get all imgs in Startsite_imgs folder
+//var startsiteimgs = Startsite_imgs.getFiles();
